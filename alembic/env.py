b2878e378913886013edb3e825f8a7cdde6ade71
ufuksh <439504@ogr.ktu.edu.tr>
2024-12-09 16:00:48 +0300

    preprod

--- alembic/env.py
+++ alembic/env.py
@@ -1,32 +1,52 @@
 # alembic/env.py
-from logging.config import fileConfig
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-from alembic import context
 import os
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
+from alembic import context
 from dotenv import load_dotenv
+
+# .env dosyasını yükleyin
 load_dotenv()
 
+# Alembic yapılandırmasını alın
 config = context.config
-fileConfig(config.config_file_name)
-target_metadata = None
 
-from app.models import Base
+# Config dosyasındaki logging ayarlarını yükleyin
+fileConfig(config.config_file_name)
+
+# Modelinizi içe aktarın
+from app.models import Base  # Model dosyanızın doğru yolunu kullanın
+
+# Hedef metadata'yı belirleyin
 target_metadata = Base.metadata
 
 def run_migrations_offline():
+    """Offline migrasyon çalıştırma."""
     url = os.getenv("DATABASE_URL")
-    context.configure(url=url, target_metadata=target_metadata, literal_binds=True, dialect_opts={"paramstyle": "named"})
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
     with context.begin_transaction():
         context.run_migrations()
 
 def run_migrations_online():
+    """Online migrasyon çalıştırma."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
         prefix='sqlalchemy.',
-        poolclass=pool.NullPool)
+        poolclass=pool.NullPool
+    )
+
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
         with context.begin_transaction():
             context.run_migrations()
 
