ff778d125b3d45f76749b51068800778de26aa44
ufuksh <439504@ogr.ktu.edu.tr>
2024-12-15 14:59:12 +0300

    prod

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, RedirectResponse
 from .routers import students, teachers, schedules, users
 
 # FastAPI uygulaması oluşturuluyor
@@ -30,6 +30,16 @@ app = FastAPI(
 )
 
 # ----------------------------
+# Statik Dosyaların Servis Edilmesi
+# ----------------------------
+# Vue.js frontend dist klasörünü servis etmek için StaticFiles kullanıyoruz.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Projenin ana dizini
+DIST_DIR = os.path.join(BASE_DIR, "frontend", "dist")  # Vue.js dist dizini
+
+# Vue dist klasörünü "/" kök dizinine mount ediyoruz.
+app.mount("/", StaticFiles(directory=DIST_DIR, html=True), name="static")
+
+# ----------------------------
 # Router'ların Dahil Edilmesi
 # ----------------------------
 # Her bir modül için API rotalarını dahil ediyoruz
@@ -39,20 +49,8 @@ app.include_router(schedules.router)  # Ders programı için CRUD işlemleri
 app.include_router(users.router)      # Kullanıcı login işlemleri
 
 # ----------------------------
-# Statik Dosyaların Servis Edilmesi
-# ----------------------------
-# Vue.js frontend dist klasörünü servis etmek için StaticFiles kullanıyoruz.
-# Bu sayede tarayıcı üzerinden Vue.js dosyaları erişilebilir.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Projenin ana dizini
-DIST_DIR = os.path.join(BASE_DIR, "frontend", "dist")  # Vue.js dist dizini
-
-# Vue dist klasörünü "/" kök dizinine mount ediyoruz.
-app.mount("/", StaticFiles(directory=DIST_DIR, html=True), name="static")
-
-# ----------------------------
 # Root Endpoint
 # ----------------------------
-# API'nin çalıştığını test etmek için basit bir root endpoint.
 @app.get("/ping", tags=["Health Check"])
 def read_root():
     """
@@ -66,10 +64,7 @@ def read_root():
 @app.get("/{full_path:path}")
 async def catch_all(full_path: str):
     """
-    Vue.js uygulamasını desteklemek için bilinmeyen rotaları yakalar ve
-    index.html dosyasını döndürür.
+    Vue.js tarafından ele alınmayan tüm rotalar için index.html döner.
     """
-    index_file = os.path.join(DIST_DIR, "index.html")
-    if os.path.exists(index_file):
-        return FileResponse(index_file)
-    return {"message": "Not Found"}, 404
+    # Direkt "/"'e yönlendirme
+    return RedirectResponse("/")
